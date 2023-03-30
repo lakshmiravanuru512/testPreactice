aabb6c0306396478086cbd4b08eab71fd4bd42b8
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-03-30 21:57:30 +0530

    counter example

---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+
+
+const Counter=()=>{
+    return (
+        <div>
+            <h1>Counter</h1>
+            <p>0</p>
+            <div>
+                <button>Increase</button>
+                <button>Decrease</button>
+            </div>
+        </div>
+    )
+}
+
+export default Counter;
